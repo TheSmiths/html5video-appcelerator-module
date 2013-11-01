6e78d0a5bb7ff5bb22c6b812daea8d18ee5df6f9
Wienke Giezeman <wgiezeman@gmail.com>
2013-11-01 22:08:32 +0100

    added example

--- example/app.js
+++ example/app.js
@@ -1,43 +1,39 @@
 // This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
+// You should do something interesting in this harness
+// to test out the module and to provide instructions
 // to users on how to use it by example.
-
 
 // open a single window
 
 // exmaple
 
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+var home = Ti.UI.createWindow({
+	backgroundColor : 'white'
 });
-var label = Ti.UI.createLabel();
+
+var button = Ti.UI.createView({
+	width : 100,
+	height : 100,
+	backgroundColor : 'red'
+});
 win.add(label);
-win.open();
 
-// TODO: write your module tests here
-var HTML5Video = require('tv.wappzapp.html5video');
-Ti.API.info("module is => " + HTML5Video);
-
-label.text = HTML5Video.example();
-
-Ti.API.info("module exampleProp is => " + HTML5Video.exampleProp);
-HTML5Video.exampleProp = "This is a test value";
-
-if (Ti.Platform.name == "android") {
-	var proxy = HTML5Video.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150,
-		url:'http://www.youtube.com/embed/ETAEcomff7g'
+button.addEventListener('click', function() {
+	var win = Ti.UI.createWindow({
+		backgroundColor : 'white',
+		fullscreen : false // Makes it heavyweight
 	});
 
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
-}
+	var HTML5Video = require('tv.wappzapp.html5video');
+	if (Ti.Platform.name == "android") {
+		var proxy = HTML5Video.createView({
+			width : 200,
+			height : 100,
+			url : 'http://www.youtube.com/embed/ETAEcomff7g'
+		});
+
+		win.add(proxy);
+	}
+	win.open();
+});
 
